e636dc6c35945ec1261cb69d791d2d628382283e
Alfian Taka <pianaee379@gmail.com>
2026-03-31 12:18:49 +0700

    refactor: Kattis - mia w/ func

--- chapter-1/1-entry-point/function/1-kattis-mia.cpp
+++ chapter-1/1-entry-point/function/1-kattis-mia.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+int getScore(int hi, int lo) {
+  if (lo == 1 && hi == 2) return 100;
+  else if (lo == hi) return 80 + hi;
+  else return hi * 10 + lo;
+}
+
 int main() {
   int s1, s2, r1, r2, sRes, rRes;
 
@@ -11,13 +17,8 @@ int main() {
     int sMax = max(s1, s2); int sMin = min(s1, s2);
     int rMax = max(r1, r2); int rMin = min(r1, r2);
 
-    if (sMin == 1 && sMax == 2) sRes = 100;
-    else if (sMin == sMax) sRes = 80 + sMax;
-    else sRes = sMax * 10 + sMin;
-
-    if (rMin == 1 && rMax == 2) rRes = 100;
-    else if (rMin == rMax) rRes = 80 + rMax;
-    else rRes = rMax * 10 + rMin;
+    sRes = getScore(sMax, sMin);
+    rRes = getScore(rMax, rMin);
 
     if (sRes > rRes) cout << "Player 1 wins.\n";
     else if (rRes > sRes) cout << "Player 2 wins.\n";
