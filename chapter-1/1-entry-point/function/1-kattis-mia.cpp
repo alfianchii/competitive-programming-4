ee9453167867f16bcd52d7b658bb727a7f741fdf
Alfian Taka <pianaee379@gmail.com>
2026-03-31 11:48:19 +0700

    feat: Kattis - mia [AC]

--- chapter-1/1-entry-point/function/1-kattis-mia.cpp
+++ chapter-1/1-entry-point/function/1-kattis-mia.cpp
@@ -8,23 +8,19 @@ int main() {
   while (cin >> s1 >> s2 >> r1 >> r2) {
     if (s1 == 0 && s2 == 0 && r1 == 0 && r2 == 0) break;
 
-    if ((s1 == 1 && s2 == 2) || (s1 == 2 && s2 == 1)) sRes = 3;
-    else if (s1 == s2) sRes = 2;
-    else sRes = 1;
+    int sMax = max(s1, s2); int sMin = min(s1, s2);
+    int rMax = max(r1, r2); int rMin = min(r1, r2);
 
-    if ((r1 == 1 && r2 == 2) || (r1 == 2 && r2 == 1)) rRes = 3;
-    else if (r1 == r2) rRes = 2;
-    else rRes = 1;
+    if (sMin == 1 && sMax == 2) sRes = 100;
+    else if (sMin == sMax) sRes = 80 + sMax;
+    else sRes = sMax * 10 + sMin;
 
-    if (sRes == rRes) {
-      int highestS = max({s1, s2});
-      int highestR = max({r1, r2});
+    if (rMin == 1 && rMax == 2) rRes = 100;
+    else if (rMin == rMax) rRes = 80 + rMax;
+    else rRes = rMax * 10 + rMin;
 
-      if (highestS > highestR) cout << "Player 1 wins.\n";
-      else if (highestR > highestS) cout << "Player 2 wins.\n";
-      else cout << "Tie.\n";
-    }
-    else if (sRes > rRes) cout << "Player 1 wins.\n";
+    if (sRes > rRes) cout << "Player 1 wins.\n";
     else if (rRes > sRes) cout << "Player 2 wins.\n";
+    else cout << "Tie.\n";
   }
 }
