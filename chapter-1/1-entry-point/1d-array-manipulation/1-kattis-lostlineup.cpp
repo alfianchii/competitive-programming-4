4e4e7967653411adee567db3f8fbb32e03fa9048
Alfian Taka <pianaee379@gmail.com>
2026-04-01 15:11:02 +0700

    refactor: Kattis - lostlineup [AC]

--- chapter-1/1-entry-point/1d-array-manipulation/1-kattis-lostlineup.cpp
+++ chapter-1/1-entry-point/1d-array-manipulation/1-kattis-lostlineup.cpp
@@ -4,22 +4,12 @@ using namespace std;
 
 int main() {
   int n; cin >> n;
-  int line[n]; line[0] = 0;
+  int line[n]; line[0] = 1;
 
-  int p;
-  int count = 1;
-  while (cin >> p) {
-    line[count] = p;
-    count++;
+  for (int i = 1; i <= n - 1; i++) {
+    int p; cin >> p;
+    line[p + 1] = i + 1;
   }
 
-  int res[n]; res[0] = 1;
-  for (int i = 1; i < n; i++) {
-    int jimmy = line[0];
-    int other = line[i] + 1;
-
-    res[jimmy + other] = i + 1;
-  }
-
-  for (int item : res) cout << item << " ";
+  for (int person : line) cout << person << " ";
 }
