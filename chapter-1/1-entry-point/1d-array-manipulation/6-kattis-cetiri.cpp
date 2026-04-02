22026e70d87f2e7f1b90893fd304d5cc8024d03b
Alfian Taka <pianaee379@gmail.com>
2026-04-02 17:26:05 +0700

    fix: Kattis - cetiri; fill the mid based on temp and lo-hi [AC]

--- chapter-1/1-entry-point/1d-array-manipulation/6-kattis-cetiri.cpp
+++ chapter-1/1-entry-point/1d-array-manipulation/6-kattis-cetiri.cpp
@@ -8,15 +8,16 @@ int main() {
 
   int lo = min({temp[0], temp[1], temp[2]});
   int hi = max({temp[0], temp[1], temp[2]});
-  int nums[4]; nums[0] = lo; nums[2] = hi;
-  for (int t : temp) {
-    if (t != lo && t != hi) nums[1] = t;
-  }
+
+  int nums[3]; 
+  nums[0] = lo;
+  nums[2] = hi;
+  nums[1] = temp[0] + temp[1] + temp[2] - lo - hi;
 
   int a = nums[1] - nums[0];
   int b = nums[2] - nums[1];
 
   if (a == b) cout << nums[2] + a;
   else if (a > b) cout << nums[0] + b;
-  else if (a < b) cout << nums[1] + a;
+  else cout << nums[1] + a;
 }
