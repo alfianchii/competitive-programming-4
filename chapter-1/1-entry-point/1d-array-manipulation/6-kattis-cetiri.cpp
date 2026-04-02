75fbb41af9fc4d0f9268c625e0cafa4bfa60a2ce
Alfian Taka <pianaee379@gmail.com>
2026-04-02 17:22:51 +0700

    feat: Kattis - cetiri [WA; -1 TC]

--- chapter-1/1-entry-point/1d-array-manipulation/6-kattis-cetiri.cpp
+++ chapter-1/1-entry-point/1d-array-manipulation/6-kattis-cetiri.cpp
@@ -3,26 +3,20 @@
 using namespace std;
 
 int main() {
-  vector<int> temp; int nums[4]; memset(nums, 0, sizeof(nums));
-  for (int i = 0; i < 3; i++) {
-    int x; cin >> x; temp.push_back(x);
-  }
+  vector<int> temp(3);
+  for (int i = 0; i < 3; i++) cin >> temp[i];
 
   int lo = min({temp[0], temp[1], temp[2]});
   int hi = max({temp[0], temp[1], temp[2]});
-  nums[0] = lo; nums[2] = hi;
+  int nums[4]; nums[0] = lo; nums[2] = hi;
   for (int t : temp) {
     if (t != lo && t != hi) nums[1] = t;
   }
-  
+
   int a = nums[1] - nums[0];
   int b = nums[2] - nums[1];
 
-  if (a != b) {
-    int diff = (nums[2] - nums[0]) / 3;
-    nums[3] = nums[2];
-    nums[2] = nums[1] + diff;
-
-    cout << nums[2];
-  } else cout << nums[2] + a;
+  if (a == b) cout << nums[2] + a;
+  else if (a > b) cout << nums[0] + b;
+  else if (a < b) cout << nums[1] + a;
 }
