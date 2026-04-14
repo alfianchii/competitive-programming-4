34d571fb7a1530b27049c4d90de1a987b8d8d443
Alfian Taka <pianaee379@gmail.com>
2026-04-14 14:25:25 +0700

    feat: UVa 10388 - Snap [WA]

---
+++ chapter-1/ad-hoc/game-card/2-uva-10388-Snap.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int tcs; cin >> tcs;
+
+  while (tcs--) {
+    // STACK: pop-to-right
+    // QUEUE: pop-to-left
+    string jane, john; cin >> jane >> john;
+    deque<char> janeDown, johnDown;
+    deque<char> janeUp, johnUp;
+    bool finish = false;
+    for (char c : jane) janeDown.push_back(c);
+    for (char c : john) johnDown.push_back(c);
+
+    for (int i = 0; i < 1000; i++) {
+      if (janeDown.empty()) {
+        while (!janeUp.empty()) {
+          janeDown.push_front(janeUp.back());
+          janeUp.pop_back();
+        }
+      }
+      if (johnDown.empty()) {
+        while (!johnUp.empty()) {
+          johnDown.push_front(johnUp.back());
+          johnUp.pop_back();
+        }
+      }
+
+      char janeCard = janeDown.front(); janeDown.pop_front();
+      char johnCard = johnDown.front(); johnDown.pop_front();
+      johnUp.push_front(johnCard);
+      janeUp.push_front(janeCard);
+
+      if (janeCard == johnCard) {
+        int verdict = random() / 141 % 2;
+
+        if (verdict == 0) {
+          while (!johnUp.empty()) {
+            janeUp.push_front(johnUp.back());
+            johnUp.pop_back();
+          }
+
+          cout << "Snap! for Jane: ";
+          for (char card : janeUp) cout << card;
+          cout << "\n";
+        }
+        else if (verdict == 1) {
+          while (!janeUp.empty()) {
+            johnUp.push_front(janeUp.back());
+            janeUp.pop_back();
+          }
+
+          cout << "Snap! for John: ";
+          for (char card : johnUp) cout << card;
+          cout << "\n";
+        }
+      }
+
+      if (janeDown.empty() && janeUp.empty()) {
+        cout << "John wins.\n";
+        finish = true;
+        break;
+      } else if (johnDown.empty() && johnUp.empty()) {
+        cout << "Jane wins.\n";
+        finish = true;
+        break;
+      }
+    }
+
+    if (!finish) cout << "Keeps going and going ...";;
+  }
+}
