eee03693c5deaa19f3bad9b4bbcbd99fec2b4978
Alfian Taka <pianaee379@gmail.com>
2026-05-20 10:41:49 +0700

    refactor: `Implement Stack Using Queues`; queue [AC]

--- Platforms/NeetCode/stack/implement_stack_using_queues/solution.go
+++ Platforms/NeetCode/stack/implement_stack_using_queues/solution.go
@@ -12,16 +12,23 @@ func Constructor() MyStack {
 
 func (this *MyStack) Push(x int) {
 	this.data = append(this.data, x)
+
+	n := len(this.data) - 1
+	for range n {
+		front := this.data[0]
+		this.data = this.data[1:]
+		this.data = append(this.data, front)
+	}
 }
 
 func (this *MyStack) Pop() int {
 	top := this.Top()
-	this.data = this.data[:len(this.data)-1]
+	this.data = this.data[1:]
 	return top
 }
 
 func (this *MyStack) Top() int {
-	return this.data[len(this.data)-1]
+	return this.data[0]
 }
 
 func (this *MyStack) Empty() bool {
