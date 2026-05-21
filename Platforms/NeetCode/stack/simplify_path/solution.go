adf58110da8bf82c1a6a881e49dc923f8fe1a1c8
Alfian Taka <pianaee379@gmail.com>
2026-05-21 14:27:08 +0700

    feat: `Simplify Path` [WA]

---
+++ Platforms/NeetCode/stack/simplify_path/solution.go
@@ -0,0 +1,84 @@
+package main
+
+import "strings"
+
+type Stack struct {
+	data []string
+}
+
+func (this *Stack) Push(val string) {
+	this.data = append(this.data, val)
+}
+
+func (this *Stack) Pop() string {
+	top := this.Top()
+	this.data = this.data[:len(this.data)-1]
+	return top
+}
+
+func (this *Stack) Top() string {
+	return this.data[len(this.data)-1]
+}
+
+func (this *Stack) IsEmpty() bool {
+	return len(this.data) == 0
+}
+
+func SimplifyPath(path string) string {
+	stack := Stack{}
+
+	symbol := ""
+	letter := ""
+	for _, str := range path {
+		switch str {
+		case '/', '.':
+			symbol += string(str)
+			if len(letter) != 0 {
+				stack.Push(letter)
+				letter = ""
+			}
+		default:
+			letter += string(str)
+			if len(symbol) != 0 {
+				if strings.Contains(symbol, "...") {
+					stack.Push("...")
+				}
+
+				if strings.Contains(symbol, "..") && !stack.IsEmpty() {
+					stack.Pop()
+				}
+
+				symbol = ""
+			}
+		}
+	}
+
+	if len(letter) != 0 {
+		stack.Push(letter)
+		letter = ""
+	}
+
+	if len(symbol) != 0 {
+		if strings.Contains(symbol, "...") {
+			stack.Push("...")
+		}
+
+		if strings.Contains(symbol, "..") && !stack.IsEmpty() {
+			stack.Pop()
+		}
+
+		symbol = ""
+	}
+
+	fullPath := ""
+	if !stack.IsEmpty() {
+		for _, pathName := range stack.data {
+			fullPath += "/"
+			fullPath += pathName
+		}
+	} else {
+		fullPath += "/"
+	}
+
+	return fullPath
+}
