7b8464d2b70e2ac9d84b0bb453ad77a30ba5c5fe
Alfian Taka <pianaee379@gmail.com>
2026-05-25 13:48:43 +0700

    refactor: `Maximum Frequency Stack` w/ grouping

--- Platforms/NeetCode/stack/maximum_frequency_stack/solution.go
+++ Platforms/NeetCode/stack/maximum_frequency_stack/solution.go
@@ -1,46 +1,42 @@
 package main
 
 type FreqStack struct {
-	stack []int
-	freq  map[int]int
+	freqs   map[int]int
+	groups  map[int][]int
+	maxFreq int
 }
 
 func Constructor() FreqStack {
 	return FreqStack{
-		stack: []int{},
-		freq:  make(map[int]int),
+		freqs:   make(map[int]int),
+		groups:  make(map[int][]int),
+		maxFreq: 0,
 	}
 }
 
 func (this *FreqStack) Push(val int) {
-	this.stack = append(this.stack, val)
-	this.freq[val]++
+	this.freqs[val]++
+	currentFreq := this.freqs[val]
+	this.groups[currentFreq] = append(this.groups[currentFreq], val)
+	if currentFreq > this.maxFreq {
+		this.maxFreq = currentFreq
+	}
 }
 
 func (this *FreqStack) Pop() int {
-	mostFreq := 0
-	mostFreqVal := 0
-	mostFreqIdx := 0
-	for idx := len(this.stack) - 1; idx >= 0; idx-- {
-		val := this.stack[idx]
-		freq := this.freq[val]
-		if freq > mostFreq {
-			mostFreq = freq
-			mostFreqIdx = idx
-			mostFreqVal = val
-		}
+	mostFreqs := this.groups[this.maxFreq]
+	val := mostFreqs[len(mostFreqs)-1]
+	this.groups[this.maxFreq] = mostFreqs[:len(mostFreqs)-1]
+	this.freqs[val]--
+
+	if this.freqs[val] == 0 {
+		delete(this.freqs, val)
 	}
 
-	if len(this.stack) > 1 {
-		this.stack = append(this.stack[:mostFreqIdx], this.stack[mostFreqIdx+1:]...)
-	} else {
-		this.stack = this.stack[:len(this.stack)-1]
+	if len(this.groups[this.maxFreq]) == 0 {
+		delete(this.groups, this.maxFreq)
+		this.maxFreq--
 	}
-	this.freq[mostFreqVal]--
 
-	return mostFreqVal
-}
-
-func (this *FreqStack) Top() int {
-	return this.stack[len(this.stack)-1]
+	return val
 }
