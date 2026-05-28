81745ccf826945384ea79fec8a4d55e4efe3f507
Alfian Taka <pianaee379@gmail.com>
2026-05-28 16:02:07 +0700

    refactor: `Design Hashset`

--- Platforms/NeetCode/arrays_hasing/design_hashset/solution.go
+++ Platforms/NeetCode/arrays_hasing/design_hashset/solution.go
@@ -1,5 +1,7 @@
 package main
 
+import "slices"
+
 type MyHashSet struct {
 	buckets [][]int
 }
@@ -33,13 +35,7 @@ func (this *MyHashSet) Remove(key int) {
 func (this *MyHashSet) Contains(key int) bool {
 	idx := this.hash(key)
 
-	for _, v := range this.buckets[idx] {
-		if v == key {
-			return true
-		}
-	}
-
-	return false
+	return slices.Contains(this.buckets[idx], key)
 }
 
 func (this *MyHashSet) hash(key int) int {
